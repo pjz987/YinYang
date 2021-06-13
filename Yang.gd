319b29c9fd85f4fdb65da149b7692d73b4153083
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-06-12 20:32:04 -0700

    :yin_yang:

--- Yang.gd
+++ Yang.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 class_name Yang
 
-export var MOVE_SPEED = 100
+export var MOVE_SPEED = 50
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_vector := get_inputs()
 	move(input_vector)
 
@@ -14,6 +14,5 @@ func get_inputs() -> Vector2:
 	).normalized() * MOVE_SPEED
 
 func move(input_vector) -> void:
-	move_and_slide(input_vector)
-#	global_position.x += input_vector.x
-#	global_position.y += input_vector.y
+	move_and_slide(Vector2(input_vector.x, 0))
+	move_and_slide(Vector2(0, input_vector.y))
